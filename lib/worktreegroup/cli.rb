efb21d6370f094db2ac8d288a6ce082bdff18ff2
nasustim <mitsuhibino@gmail.com>
2025-07-12 15:19:39 +0900

    Remove unnecesary functions

--- lib/worktreegroup/cli.rb
+++ lib/worktreegroup/cli.rb
@@ -4,10 +4,6 @@ require "thor"
 
 module Worktreegroup
   class CLI < Thor
-    def self.exit_on_failure?
-      true
-    end
-
     default_task :hello
 
     desc "hello", "Show greeting message"
@@ -18,37 +14,6 @@ module Worktreegroup
     desc "version", "Show version"
     def version
       puts VERSION
-    end
-
-    desc "list", "List Git worktrees"
-    def list
-      puts "Listing Git worktrees..."
-      # TODO: Implement worktree listing functionality
-    end
-
-    desc "group COMMAND ...ARGS", "Manage worktree groups"
-    subcommand "group", Group
-
-    private
-
-    class Group < Thor
-      desc "create NAME", "Create a new worktree group"
-      def create(name)
-        puts "Creating worktree group: #{name}"
-        # TODO: Implement group creation functionality
-      end
-
-      desc "list", "List all worktree groups"
-      def list
-        puts "Listing worktree groups..."
-        # TODO: Implement group listing functionality
-      end
-
-      desc "add GROUP WORKTREE", "Add worktree to group"
-      def add(group, worktree)
-        puts "Adding worktree '#{worktree}' to group '#{group}'"
-        # TODO: Implement adding worktree to group
-      end
     end
   end
 end
